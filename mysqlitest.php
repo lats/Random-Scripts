86f6d08d78457b95634d95f62103ec0a8c205564
Bill <aardvark@zzyzx.info>
2018-09-06 12:22:52 -0500

    Update mysqlitest.php

--- mysqlitest.php
+++ mysqlitest.php
@@ -1,8 +1,12 @@
 <?php
-$dbuser = "username";
-$dbpass = "password";
-$dbhost = "localhost";
-$dbname = "database";
+echo "Testing";
+require_once('wp-config.php');
+
+
+$dbuser = DB_USER;
+$dbpass = DB_PASSWORD;
+$dbhost = DB_HOST;
+$dbname = DB_NAME;
 $sql = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die(mysqli_error());
 $query = "show tables";
 $result = mysqli_query($sql,$query) or die(mysqli_error($sql));
